f30a6e6bbd39982b28bd6a3529eb44ba8b815b3c
Eva van Geenen <EvaG@admins-MacBook-Pro.local>
2020-08-05 15:43:05 +0200

    Alles vervangende SpringBootApllication

--- chapter1/src/main/java/eva/recipes/chapter1/demo/DemoApplication.java
+++ chapter1/src/main/java/eva/recipes/chapter1/demo/DemoApplication.java
@@ -2,15 +2,14 @@ package eva.recipes.chapter1.demo;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+//@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
