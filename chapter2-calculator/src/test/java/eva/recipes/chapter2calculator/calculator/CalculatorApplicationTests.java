2bd8f4dc7ecde4d5c8526d44b0507a901b949fbe
Eva van Geenen <evavangeenen@gmail.com>
2020-08-07 17:01:30 +0200

    Using MockBeans, but tests don't work completely yet.

--- chapter2-calculator/src/test/java/eva/recipes/chapter2calculator/calculator/CalculatorApplicationTests.java
+++ chapter2-calculator/src/test/java/eva/recipes/chapter2calculator/calculator/CalculatorApplicationTests.java
@@ -4,9 +4,14 @@ import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.system.OutputCaptureRule;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest(classes = CalculatorApplication.class)
@@ -15,8 +20,17 @@ public class CalculatorApplicationTests {
     @Rule
     public OutputCaptureRule capture = new OutputCaptureRule();
 
-    @Autowired
+    @MockBean
     private Calculator calculator;
+
+    @MockBean(name = "multiplyOperation")
+    private Operation mockOperation;
+
+    @Test
+    public void calculatorShouldHave3Operations(){
+        Object operations = ReflectionTestUtils.getField(calculator, "operations");
+        assertThat((Collection)operations).hasSize(2);
+    }
 
     @Test
     public void doingMultiplicationsShouldSucceed(){
