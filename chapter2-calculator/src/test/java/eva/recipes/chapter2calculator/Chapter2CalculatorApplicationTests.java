d6a5c4139a1066064cc8c8e7c67bca2579f940e2
Eva van Geenen <evavangeenen@gmail.com>
2020-08-07 11:20:41 +0200

    Moved calculater to a new maven project

---
+++ chapter2-calculator/src/test/java/eva/recipes/chapter2calculator/Chapter2CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package eva.recipes.chapter2calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter2CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
