cedbaddaf84ff10e45351bc18ac4981bacf8d753
Eva van Geenen <evavangeenen@gmail.com>
2020-08-07 16:07:37 +0200

    Testing with rule

--- chapter2-calculator/src/test/java/eva/recipes/chapter2calculator/calculator/CalculatorApplicationTests.java
+++ chapter2-calculator/src/test/java/eva/recipes/chapter2calculator/calculator/CalculatorApplicationTests.java
@@ -1,18 +1,28 @@
 package eva.recipes.chapter2calculator.calculator;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.system.OutputCaptureRule;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest(classes = CalculatorApplication.class)
-class CalculatorApplicationTest {
+public class CalculatorApplicationTests {
+
+    @Rule
+    public OutputCaptureRule capture = new OutputCaptureRule();
 
     @Autowired
     private Calculator calculator;
+
+    @Test
+    public void doingMultiplicationsShouldSucceed(){
+        calculator.calculate(12,13, '*');
+        capture.expect(Matchers.containsString("12 * 13 = 156"));
+    }
 
     @Test
     public void doingDivisionShouldFail(){
