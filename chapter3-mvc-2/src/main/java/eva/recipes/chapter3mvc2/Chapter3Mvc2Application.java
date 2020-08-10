2843363c8561082e820c73adf64bf2fe85bdac9a
Eva van Geenen <evavangeenen@gmail.com>
2020-08-10 11:24:19 +0200

    Chapter3-mvc-2 Books and Library project init. First commit.

---
+++ chapter3-mvc-2/src/main/java/eva/recipes/chapter3mvc2/Chapter3Mvc2Application.java
@@ -0,0 +1,13 @@
+package eva.recipes.chapter3mvc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter3Mvc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter3Mvc2Application.class, args);
+	}
+
+}
