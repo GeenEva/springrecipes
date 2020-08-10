5941a62c68cc794d2612e9195903f85a390ad0b5
Eva van Geenen <evavangeenen@gmail.com>
2020-08-10 11:27:01 +0200

    Adding class Book and refactor name of the application class to LibraryApplication.

--- chapter3-mvc-2/src/main/java/eva/recipes/chapter3mvc2/LibraryApplication.java
+++ chapter3-mvc-2/src/main/java/eva/recipes/chapter3mvc2/LibraryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chapter3Mvc2Application {
+public class LibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Chapter3Mvc2Application.class, args);
+		SpringApplication.run(LibraryApplication.class, args);
 	}
 
 }
---
+++ chapter3-mvc-2/src/main/java/eva/recipes/chapter3mvc2/library/Book.java
@@ -0,0 +1,6 @@
+package eva.recipes.chapter3mvc2.library;
+
+public class Book {
+
+
+}
