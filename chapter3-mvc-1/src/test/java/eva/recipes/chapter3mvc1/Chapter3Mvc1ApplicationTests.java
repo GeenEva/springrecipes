e52434f2e214f4698102df8781f1c6e650260e75
Eva van Geenen <evavangeenen@gmail.com>
2020-08-10 10:27:41 +0200

    Init chapter3-mvc-1 First commit

---
+++ chapter3-mvc-1/src/test/java/eva/recipes/chapter3mvc1/Chapter3Mvc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package eva.recipes.chapter3mvc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter3Mvc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
