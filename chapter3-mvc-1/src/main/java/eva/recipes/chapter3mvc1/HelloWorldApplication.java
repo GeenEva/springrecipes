2b76d2bcbb556442f3264504afeab9c96491ade2
Eva van Geenen <evavangeenen@gmail.com>
2020-08-10 10:29:55 +0200

    Rename of application class.

--- chapter3-mvc-1/src/main/java/eva/recipes/chapter3mvc1/HelloWorldApplication.java
+++ chapter3-mvc-1/src/main/java/eva/recipes/chapter3mvc1/HelloWorldApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chapter3Mvc1Application {
+public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(Chapter3Mvc1Application.class, args);
+		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
 }
