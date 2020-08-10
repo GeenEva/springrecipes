1b33c3e9e69cc23567e703e2416c7af6da00ce80
Eva van Geenen <evavangeenen@gmail.com>
2020-08-10 17:40:23 +0200

    Added BookControllerTest class and test for finding all books.

---
+++ chapter3-mvc-2/src/test/java/eva/recipes/chapter3mvc2/library/BookControllerTest.java
@@ -0,0 +1,43 @@
+package eva.recipes.chapter3mvc2.library;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.Mockito.when;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest
+class BookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    public void shouldReturnListOfBooks() throws Exception {
+        when(bookService.findAll()).thenReturn(Arrays.asList(
+                new Book("123", "Spring 5 Recipes", "Marten Deinum", "Josh Long"),
+                new Book("456", "Pro Spring MVC", "Marten Deinum", "Colin Yates")
+                ));
+
+        mockMvc.perform(get("/books"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2) ))
+                .andExpect(jsonPath("$[*].isbn", containsInAnyOrder("123", "456")))
+                .andExpect(jsonPath("$[*].title", containsInAnyOrder("Spring 5 Recipes", "Pro Spring MVC" )));
+
+    }
+
+}
