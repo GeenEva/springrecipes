098da95ff25368233f2146b2dfca4370aa71526a
Eva van Geenen <evavangeenen@gmail.com>
2020-08-11 11:41:20 +0200

    Moved chapter 3-3 adaptations to new project folder.

---
+++ chapter3-mvc-3/src/main/java/eva/recipes/chapter3mvc2/library/BookController.java
@@ -0,0 +1,46 @@
+package eva.recipes.chapter3mvc2.library;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+   
+    public Iterable<Book> list() {
+        return bookService.findAll();
+    }
+
+
+    @GetMapping("/books.html")
+    public String all(Model model){
+        model.addAttribute("books", bookService.findAll());
+        return "books/list";
+    }
+
+    @GetMapping("/{isbn}")
+    public ResponseEntity<Book> getBookByIsbn(@PathVariable("isbn") String isbn){
+        return bookService.find(isbn)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book, UriComponentsBuilder uriBuilder){
+        Book created = bookService.create(book);
+        URI newBookUri = uriBuilder.path("/books/{isbn}").build(created.getIsbn());
+        return ResponseEntity.created(newBookUri).body(created);
+    }
+}
