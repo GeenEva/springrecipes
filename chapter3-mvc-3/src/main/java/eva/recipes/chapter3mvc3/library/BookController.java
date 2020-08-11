6900715353f84deb7e7f4faa3bf7649bd8acd6d6
Eva van Geenen <evavangeenen@gmail.com>
2020-08-11 13:07:32 +0200

    Added view for books/list and controller method for it.

--- chapter3-mvc-3/src/main/java/eva/recipes/chapter3mvc3/library/BookController.java
+++ chapter3-mvc-3/src/main/java/eva/recipes/chapter3mvc3/library/BookController.java
@@ -1,26 +1,17 @@
 package eva.recipes.chapter3mvc3.library;
 
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import java.net.URI;
-
-@RestController
-@RequestMapping("/books")
+@Controller
 public class BookController {
 
     private final BookService bookService;
 
     public BookController(BookService bookService) {
         this.bookService = bookService;
-    }
-
-   
-    public Iterable<eva.recipes.chapter3mvc3.library.Book> list() {
-        return bookService.findAll();
     }
 
 
@@ -30,17 +21,4 @@ public class BookController {
         return "books/list";
     }
 
-    @GetMapping("/{isbn}")
-    public ResponseEntity<eva.recipes.chapter3mvc3.library.Book> getBookByIsbn(@PathVariable("isbn") String isbn){
-        return bookService.find(isbn)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
-    }
-
-    @PostMapping
-    public ResponseEntity<Book> createBook(@RequestBody Book book, UriComponentsBuilder uriBuilder){
-        Book created = bookService.create(book);
-        URI newBookUri = uriBuilder.path("/books/{isbn}").build(created.getIsbn());
-        return ResponseEntity.created(newBookUri).body(created);
-    }
 }
