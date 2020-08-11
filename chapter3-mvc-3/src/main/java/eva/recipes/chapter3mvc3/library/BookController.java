b4aef2a48255a83d8ca9cbb355b34f492196fd73
Eva van Geenen <evavangeenen@gmail.com>
2020-08-11 15:16:20 +0200

    Added details html file and method in controller to get the book by isbn and send it to datails view

--- chapter3-mvc-3/src/main/java/eva/recipes/chapter3mvc3/library/BookController.java
+++ chapter3-mvc-3/src/main/java/eva/recipes/chapter3mvc3/library/BookController.java
@@ -4,6 +4,7 @@ package eva.recipes.chapter3mvc3.library;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookController {
@@ -19,6 +20,13 @@ public class BookController {
     public String all(Model model){
         model.addAttribute("books", bookService.findAll());
         return "books/list";
+    }
+
+    @GetMapping(value = "/books.html", params = "isbn")
+    public String getByIsbn(@RequestParam("isbn") String isbn, Model model){
+        bookService.find(isbn)
+                .ifPresent(book -> model.addAttribute("book", book));
+        return "/books/details";
     }
 
 }
