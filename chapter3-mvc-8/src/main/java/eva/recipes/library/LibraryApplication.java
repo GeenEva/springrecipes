278f0e1fb1b990566c0770efba322a3170626246
Eva van Geenen <evavangeenen@gmail.com>
2020-08-25 14:10:20 +0200

    Copied files for startup chapter.

--- chapter3-mvc-8/src/main/java/eva/recipes/library/LibraryApplication.java
+++ chapter3-mvc-8/src/main/java/eva/recipes/library/LibraryApplication.java
@@ -1,13 +1,13 @@
-package eva.recipes.chapter3mvc8;
+package eva.recipes.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chapter3Mvc8Application {
+public class LibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Chapter3Mvc8Application.class, args);
+		SpringApplication.run(LibraryApplication.class, args);
 	}
 
 }
--- chapter3-mvc-8/src/test/java/eva/recipes/chapter3mvc8/LibraryApplicationTests.java
+++ chapter3-mvc-8/src/test/java/eva/recipes/chapter3mvc8/LibraryApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Chapter3Mvc8ApplicationTests {
+class LibraryApplicationTests {
 
 	@Test
 	void contextLoads() {
