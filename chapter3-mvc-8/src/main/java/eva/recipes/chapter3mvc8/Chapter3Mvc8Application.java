db0935f072117392a363c18f5925bca56bfd7a12
Eva van Geenen <evavangeenen@gmail.com>
2020-08-25 13:55:17 +0200

    Added startup for chapter3-8

---
+++ chapter3-mvc-8/src/main/java/eva/recipes/chapter3mvc8/Chapter3Mvc8Application.java
@@ -0,0 +1,13 @@
+package eva.recipes.chapter3mvc8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter3Mvc8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter3Mvc8Application.class, args);
+	}
+
+}
---
+++ chapter3-mvc-8/src/test/java/eva/recipes/chapter3mvc8/Chapter3Mvc8ApplicationTests.java
@@ -0,0 +1,13 @@
+package eva.recipes.chapter3mvc8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter3Mvc8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
