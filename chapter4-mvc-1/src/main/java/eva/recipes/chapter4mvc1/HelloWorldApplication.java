cff07c10532b3a1831a7858d56d97d5224e3935c
Eva van Geenen <evavangeenen@gmail.com>
2020-08-28 09:47:51 +0200

    Chapter 4 setup: Spring MVC Async

---
+++ chapter4-mvc-1/src/main/java/eva/recipes/chapter4mvc1/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package eva.recipes.chapter4mvc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter4Mvc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter4Mvc1Application.class, args);
+	}
+
+}
---
+++ chapter4-mvc-1/src/test/java/eva/recipes/chapter4mvc1/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package eva.recipes.chapter4mvc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter4Mvc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
