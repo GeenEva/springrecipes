4eadc122de7e63f997ac2aa2b40dfaf7f382e1e3
Eva van Geenen <evavangeenen@gmail.com>
2020-08-28 12:35:17 +0200

    First commit chapter 4-2

---
+++ chapter4-mvc-2/src/test/java/eva/recipes/chapter4mvc2/Chapter4Mvc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package eva.recipes.chapter4mvc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter4Mvc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
