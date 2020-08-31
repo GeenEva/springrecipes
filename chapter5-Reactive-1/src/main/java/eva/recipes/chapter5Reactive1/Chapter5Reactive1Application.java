c0610a8698e61d2456bfb3a2331bacf2286998fa
Eva van Geenen <evavangeenen@gmail.com>
2020-08-31 10:18:17 +0200

    First commit Developing Reactive app with Webflux

---
+++ chapter5-Reactive-1/src/main/java/eva/recipes/chapter5Reactive1/Chapter5Reactive1Application.java
@@ -0,0 +1,13 @@
+package eva.recipes.chapter5Reactive1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter5Reactive1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter5Reactive1Application.class, args);
+	}
+
+}
---
+++ chapter5-Reactive-1/src/test/java/eva/recipes/chapter5Reactive1/Chapter5Reactive1ApplicationTests.java
@@ -0,0 +1,13 @@
+package eva.recipes.chapter5Reactive1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter5Reactive1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
