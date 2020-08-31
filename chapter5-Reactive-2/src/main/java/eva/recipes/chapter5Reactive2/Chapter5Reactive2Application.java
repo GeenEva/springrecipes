56c328bf15588dd3317af53626bac9a976f8be82
Eva van Geenen <evavangeenen@gmail.com>
2020-08-31 15:11:37 +0200

    First Commit chapter 5-2 Publishing and consuming with reactive REST services

---
+++ chapter5-Reactive-2/src/main/java/eva/recipes/chapter5Reactive2/Chapter5Reactive2Application.java
@@ -0,0 +1,13 @@
+package eva.recipes.chapter5Reactive2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter5Reactive2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter5Reactive2Application.class, args);
+	}
+
+}
---
+++ chapter5-Reactive-2/src/test/java/eva/recipes/chapter5Reactive2/Chapter5Reactive2ApplicationTests.java
@@ -0,0 +1,13 @@
+package eva.recipes.chapter5Reactive2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter5Reactive2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
