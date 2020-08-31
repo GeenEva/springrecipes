4a2dc4df567f72ae9c6b8b580ea181b5d48a7f23
Eva van Geenen <evavangeenen@gmail.com>
2020-08-31 15:44:04 +0200

    Added Order, OrderService and OrderGenerator.

---
+++ chapter5-Reactive-2/src/main/java/eva/recipes/chapter5Reactive2/OrderService.java
@@ -0,0 +1,44 @@
+package eva.recipes.chapter5Reactive2;
+
+
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class OrderService {
+
+    private final Map<String, Order> orders = new ConcurrentHashMap<>();
+
+    @PostConstruct
+    public void init(){
+        OrderGenerator generator = new OrderGenerator();
+        for (int i = 0 ; i<25 ; i++){
+            Order order = generator.generate();
+            orders.put(order.getId(), order);
+        }
+    }
+
+    public Mono<Order> findById(String id){
+        return Mono.justOrEmpty(orders.get(id));
+    }
+
+    public Mono<Order> save(Mono<Order> order){
+        return order.map(this::sayve);
+    }
+
+    private Order sayve(Order order){
+        orders.put(order.getId(), order);
+        return order;
+    }
+
+    public Flux<Order> orders(){
+        return Flux.fromIterable(orders.values()).delayElements(Duration.ofMillis(128));
+    }
+
+}
