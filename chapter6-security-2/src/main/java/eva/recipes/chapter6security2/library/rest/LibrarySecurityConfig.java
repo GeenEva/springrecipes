7599ef67d6c81c8ee471a800c68b614a0e886d2b
Eva van Geenen <evavangeenen@gmail.com>
2020-09-04 11:35:51 +0200

    Security disabled default config and then enabled some features, but without logical security dus. So you can go to the books page without authentication needed now. But login works.

--- chapter6-security-2/src/main/java/eva/recipes/chapter6security2/library/rest/LibrarySecurityConfig.java
+++ chapter6-security-2/src/main/java/eva/recipes/chapter6security2/library/rest/LibrarySecurityConfig.java
@@ -3,20 +3,31 @@ package eva.recipes.chapter6security2.library.rest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class LibrarySecurityConfig extends WebSecurityConfigurerAdapter {
+public class LibrarySecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer{
 
     public LibrarySecurityConfig(){
         super(true);
     }
 
     @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html").setViewName("login");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.securityContext()
-                .and()
-                .exceptionHandling();
-
+        http
+                .securityContext()
+                .and().exceptionHandling()
+                .and().servletApi()
+                .and().httpBasic()
+                .and().formLogin().loginPage("/login.html")
+                .and().csrf()
+        ;
     }
 }
