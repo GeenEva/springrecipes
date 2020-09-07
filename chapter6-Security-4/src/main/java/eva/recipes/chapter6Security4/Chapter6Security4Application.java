eecba9903925283151f20f30dcf90c54639774e3
Eva van Geenen <evavangeenen@gmail.com>
2020-09-07 09:30:37 +0200

    First commit chapter 6-4 Making Access Controll Decissions.

---
+++ chapter6-Security-4/src/main/java/eva/recipes/chapter6Security4/Chapter6Security4Application.java
@@ -0,0 +1,13 @@
+package eva.recipes.chapter6Security4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter6Security4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter6Security4Application.class, args);
+	}
+
+}
---
+++ chapter6-Security-4/src/test/java/eva/recipes/chapter6Security4/Chapter6Security4ApplicationTests.java
@@ -0,0 +1,13 @@
+package eva.recipes.chapter6Security4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter6Security4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
